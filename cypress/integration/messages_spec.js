885587b421b49a2b11c9015e2a2d12d8de1fee8c
Rick <rb.ricardo01@gmail.com>
2021-12-14 14:25:04 -0300

    test: add cypress test

--- cypress/integration/messages_spec.js
+++ cypress/integration/messages_spec.js
@@ -1,7 +1,8 @@
 describe('message', () => {
   it('user can send a message', () => {
     cy.visit('http://localhost:3000')
-    cy.findByRole('textbox').type('Hello')
+    cy.findByRole('textbox').type('Hello Botty')
     cy.findByRole('button', { name: /send/i }).click({ force: true })
+    cy.get('div:contains("Hello Botty")').should('exist')
   })
 })
